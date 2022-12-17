2c95f76cedbcc6f1313c40c1aea1bce6b078816a
Ekaterina <ekaterina.shnaygina02@mail.ru>
2022-12-17 22:42:54 +0300

    add Example 004 If & Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "я")
+{
+   Console.WriteLine("Самая топовая девушка прибыла!) Приветики)");
+}
+else
+{
+    Console.Write ("Добро пожаловать, ");
+    Console.Write (username);
+}
